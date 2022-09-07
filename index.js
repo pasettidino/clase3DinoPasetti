aa39de6a8725c748c6f12cb65db5a6261f183bef
Dino Pasetti <pasettidino@gmail.com>
2022-09-07 17:47:09 -0300

    Primer entregable de js

---
+++ index.js
@@ -0,0 +1,9 @@
+let entrada = prompt("Ingrese el nombre del mejor jugador de basquet argentino");
+
+while(entrada != "Manu Ginobili"){
+    alert("Jugador incorrecto");
+
+    entrada = prompt("Ingrese el nombre del mejor jugador de basquet argentino");
+}
+
+alert("Bienvenido al Salon de la fama de la NBA")
